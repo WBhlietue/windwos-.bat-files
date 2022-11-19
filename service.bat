29ca2d0ac84aa9b90ce99469999e3970d4b6dcc9
HakuRan <WBhlietue@gmail.com>
2022-11-19 01:29:46 -0800

    Update service.bat

--- service.bat
+++ service.bat
@@ -1,175 +1,338 @@
 sc config WpcMonSvc start=disabled
+sc stop WpcMonSvc
 sc config TroubleshootingSvc start=disabled
+sc stop TroubleshootingSvc
 sc config SharedRealitySvc start=disabled
-sc config BTAGService start=disabled
-sc config BluetoothUserService_2322ed start=disabled
-sc config bthserv start=disabled
+sc stop SharedRealitySvc
 sc config VacSvc start=disabled
-sc config CDPSvc start=disabled
+sc stop VacSvc
 sc config RetailDemo start=disabled
+sc stop RetailDemo
 sc config embeddedmode start=disabled
+sc stop embeddedmode
 sc config dmwappushservice start=disabled
+sc stop dmwappushservice
 sc config DmEnrollmentSvc start=disabled
+sc stop DmEnrollmentSvc
 sc config autotimesvc start=disabled
+sc stop autotimesvc
 sc config DusmSvc start=disabled
+sc stop DusmSvc
 sc config OneSyncSvc_2322ed start=disabled
+sc stop OneSyncSvc_2322ed
 sc config RmSvc start=disabled
+sc stop RmSvc
 sc config DispBrokerDesktopSvc start=disabled
+sc stop DispBrokerDesktopSvc
 sc config LxpSvc start=disabled
+sc stop LxpSvc
 sc config tzautoupdate start=disabled
+sc stop tzautoupdate
 sc config NaturalAuthentication start=disabled
+sc stop NaturalAuthentication
 sc config SEMgrSvc start=disabled
+sc stop SEMgrSvc
 sc config XblGameSave start=disabled
+sc stop XblGameSave
 sc config XboxNetApiSvc start=disabled
+sc stop XboxNetApiSvc
 sc config XblAuthManager start=disabled
+sc stop XblAuthManager
 sc config XboxGipSvc start=disabled
+sc stop XboxGipSvc
 sc config AxInstSV start=disabled
+sc stop AxInstSV
 sc config AarSvc_2322ed start=disabled 
+sc stop AarSvc_2322ed
 sc config AJRouter start=disabled
+sc stop AJRouter
 sc config ALG start=disabled
+sc stop ALG
 sc config AppMgmt start=disabled
+sc stop AppMgmt
 sc config AppXSvc start=disabled
+sc stop AppXSvc
 sc config AssignedAccessManagerSvc start=disabled
+sc stop AssignedAccessManagerSvc
 sc config BthAvctpSvcstart=disabled
+sc stop BthAvctpSvcstart
 sc config BITS start=disabled
+sc stop BITS
 sc config PeerDistSvc start=disabled
+sc stop PeerDistSvc
 sc config CaptureService_2322edc start=disabled
+sc stop CaptureService_2322edc
 sc config CertPropSvc start=disabled
+sc stop CertPropSvc
 sc config ClipSVC start=disabled
+sc stop ClipSVC
 sc config DiagTrack start=disabled
+sc stop DiagTrack
 sc config ConsentUxUserSvc_2322ed start=disabled
+sc stop ConsentUxUserSvc_2322ed
 sc config PimIndexMaintenanceSvc_2322ed start=disabled
+sc stop PimIndexMaintenanceSvc_2322ed
 sc config CredentialEnrollmentManagerUserSvc_2322ed start=disabled
+sc stop CredentialEnrollmentManagerUserSvc_2322ed
 sc config CryptSvc start=disabled
+sc stop CryptSvc
 sc config DsSvc start=disabled
+sc stop DsSvc
 sc config Delivery Optimizationc start=disabled
+sc stop Delivery
 sc config DeviceAssociationService start=disabled
+sc stop DeviceAssociationService
 sc config DeviceAssociationBrokerSvc_2322ed start=disabled
+sc stop DeviceAssociationBrokerSvc_2322ed
 sc config DevicePickerUserSvc_2322ed start=disabled
+sc stop DevicePickerUserSvc_2322ed
 sc config DevicesFlowUserSvc_2322ed start=disabled
+sc stop DevicesFlowUserSvc_2322ed
 sc config DevQueryBroker start=disabled
+sc stop DevQueryBroker
 sc config Diagnostic Execution Service start=disabled
+sc stop Diagnostic
 sc config DPS start=disabled
+sc stop DPS
 sc config TrkWks start=disabled
+sc stop TrkWks
 sc config MSDTC start=disabled
+sc stop MSDTC
 sc config EFS start=disabled
+sc stop EFS
 sc config EntAppSvc start=disabled
+sc stop EntAppSvc
 sc config Eaphost start=disabled
+sc stop Eaphost
 sc config FMAPOService start=disabled
+sc stop FMAPOService
 sc config fdPHost start=disabled
+sc stop fdPHost
 sc config FDResPub start=disabled
+sc stop FDResPub
 sc config BcastDVRUserService_2322ed start=disabled
+sc stop BcastDVRUserService_2322ed
 sc config lfsvc start=disabled
+sc stop lfsvc
 sc config GraphicsPerfSvc start=disabled
+sc stop GraphicsPerfSvc
 sc config HvHost start=disabled
+sc stop HvHost
 sc config vmickvpexchange start=disabled
+sc stop vmickvpexchange
 sc config vmicguestinterface start=disabled
+sc stop vmicguestinterface
 sc config vmicshutdown start=disabled
+sc stop vmicshutdown
 sc config vmicheartbeat start=disabled
+sc stop vmicheartbeat
 sc config vmicvmsession start=disabled
+sc stop vmicvmsession
 sc config vmictimesync start=disabled
+sc stop vmictimesync
 sc config vmicvss start=disabled
+sc stop vmicvss
 sc config vmicrdvstart=disabled
+sc stop vmicrdvstart
 sc config ibtsiva start=disabled
+sc stop ibtsiva
 sc config IntelAudioService start=disabled
+sc stop IntelAudioService
 sc config cplspcon start=disabled
+sc stop cplspcon
 sc config cphs start=disabled
+sc stop cphs
 sc config SharedAccessstart=disabled
+sc stop SharedAccessstart
 sc config iphlpsvc start=disabled
+sc stop iphlpsvc
 sc config IpxlatCfgSvc start=disabled
+sc stop IpxlatCfgSvc
 sc config MessagingService_2322ed start=disabled
+sc stop MessagingService_2322ed
 sc config diagnosticshub.standardcollector.service start=disabled
+sc stop diagnosticshub
 sc config wlidsvc start=disabled
+sc stop wlidsvc
 sc config AppVClient start=disabled
+sc stop AppVClient
 sc config MSiSCSI start=disabled
+sc stop MSiSCSI
 sc config SmsRouter start=disabled
+sc stop SmsRouter
 sc config Netlogon start=disabled
+sc stop Netlogon
 sc config NcdAutoSetup start=disabled
-sc config NcbService start=disabled
+sc stop NcdAutoSetup
 sc config Netman start=disabled
+sc stop Netman
 sc config CscService start=disabled
+sc stop CscService
 sc config PNRPsvc start=disabled
+sc stop PNRPsvc
 sc config p2psvc start=disabled
+sc stop p2psvc
 sc config p2pimsvc start=disabled
+sc stop p2pimsvc
 sc config PhoneSvc start=disabled
+sc stop PhoneSvc
 sc config PNRPAutoReg start=disabled
+sc stop PNRPAutoReg
 sc config WPDBusEnum start=disabled
+sc stop WPDBusEnum
 sc config Spooler start=disabled
+sc stop Spooler
 sc config PrintNotify start=disabled
+sc stop PrintNotify
 sc config PrintWorkflowUserSvc_2322ed start=disabled
+sc stop PrintWorkflowUserSvc_2322ed
 sc config wercplsupport start=disabled
+sc stop wercplsupport
 sc config PcaSvc start=disabled
+sc stop PcaSvc
 sc config QWAVE start=disabled
+sc stop QWAVE
 sc config RtkAudioUniversalService start=disabled
+sc stop RtkAudioUniversalService
 sc config RasAuto start=disabled
+sc stop RasAuto
 sc config RasMan start=disabled
+sc stop RasMan
 sc config TermService start=disabled
+sc stop TermService
 sc config UmRdpService start=disabled
+sc stop UmRdpService
 sc config RemoteRegistry start=disabled
+sc stop RemoteRegistry
 sc config RemoteAccess start=disabled
+sc stop RemoteAccess
 sc config seclogon start=disabled
+sc stop seclogon
 sc config SstpSvc start=disabled
+sc stop SstpSvc
 sc config SensorDataService start=disabled
+sc stop SensorDataService
 sc config SensrSvc start=disabled
+sc stop SensrSvc
 sc config SensorService start=disabled
+sc stop SensorService
 sc config LanmanServer start=disabled
+sc stop LanmanServer
 sc config shpamsvc start=disabled
+sc stop shpamsvc
 sc config ShellHWDetection start=disabled
+sc stop ShellHWDetection
 sc config SCardSvr start=disabled
+sc stop SCardSvr
 sc config ScDeviceEnum start=disabled
+sc stop ScDeviceEnum
 sc config SCPolicySvc start=disabled
+sc stop SCPolicySvc
 sc config SNMPTRAP start=disabled
+sc stop SNMPTRAP
 sc config SSDPSRV start=disabled
+sc stop SSDPSRV
 sc config StateRepository start=disabled
+sc stop StateRepository
 sc config WiaRpc start=disabled
-sc config lmhosts start=disabled
+sc stop WiaRpc
 sc config TapiSrv start=disabled
+sc stop TapiSrv
 sc config Themes start=disabled
+sc stop Themes
 sc config UserDataSvc_2322ed start=disabled
+sc stop UserDataSvc_2322ed
 sc config UnistoreSvc_2322ed start=disabled
+sc stop UnistoreSvc_2322ed
 sc config UevAgentService start=disabled
+sc stop UevAgentService
 sc config WalletService start=disabled
+sc stop WalletService
 sc config WebClient start=disabled
+sc stop WebClient
 sc config WarpJITSvc start=disabled
+sc stop WarpJITSvc
 sc config TokenBroker start=disabled
+sc stop TokenBroker
 sc config WbioSrvc start=disabled
+sc stop WbioSrvc
 sc config FrameServer start=disabled
+sc stop FrameServer
 sc config wcncsvc start=disabled
+sc stop wcncsvc
 sc config  WEPHOSTSVC start=disabled
+sc stop WEPHOSTSVC
 sc config WdNisSvc start=disabled
+sc stop WdNisSvc
 sc config FontCache start=disabled
+sc stop FontCache
 sc config Wecsvc start=disabled
+sc stop Wecsvc
 sc config stisvc start=disabled
+sc stop stisvc
 sc config WMPNetworkSvc start=disabled
+sc stop WMPNetworkSvc
 sc config PushToInstall start=disabled
+sc stop PushToInstall
 sc config WSearch start=disabled
+sc stop WSearch
 sc config W32Time start=disabled
+sc stop W32Time
 sc config spectrum start=disabled
+sc stop spectrum
 sc config perceptionsimulation start=disabled
+sc stop perceptionsimulation
 sc config WManSvc start=disabled
+sc stop WManSvc
 sc config icssvc start=disabled
+sc stop icssvc
 sc config wisvc start=disabled
+sc stop wisvc
 sc config WinHttpAutoProxySvc start=disabled
+sc stop WinHttpAutoProxySvc
 sc config dot3svc start=disabled
+sc stop dot3svc
 sc config WFDSConMgrSvc start=disabled
+sc stop WFDSConMgrSvc
 sc config workfolderssvc start=disabled
+sc stop workfolderssvc
 sc config SysMain start=disabled
+sc stop SysMain
 sc config GoogleChromeElevationService start=disabled
+sc stop GoogleChromeElevationService
 sc config gupdate start=disabled
+sc stop gupdate
 sc config gupdatem start=disabled
+sc stop gupdatem
 sc config HpTouchpointAnalyticsService start=disabled
+sc stop HpTouchpointAnalyticsService
 sc config HPAppHelperCap start=disabled
+sc stop HPAppHelperCap
 sc config HPDiagsCap start=disabled
+sc stop HPDiagsCap
 sc config HPNetworkCap start=disabled
+sc stop HPNetworkCap
 sc config HPOmenCap start=disabled
+sc stop HPOmenCap
 sc config HPSysInfoCap start=disabled
+sc stop HPSysInfoCap
 sc config jhi_service start=disabled
+sc stop jhi_service
 sc config igccservice start=disabled
+sc stop igccservice
 sc config igfxCUIService2.0.0.0 start=disabled
+sc stop igfxCUIService2
 sc config LMS start=disabled
+sc stop LMS
 sc config iaStorAfsService start=disabled
+sc stop iaStorAfsService
 sc config RstMwService start=disabled
+sc stop RstMwService
 sc config MicrosoftEdgeElevationService start=disabled
+sc stop MicrosoftEdgeElevationService
 sc config edgeupdate start=disabled
+sc stop edgeupdate
 sc config edgeupdatem start=disabled
+sc stop edgeupdatem
 sc config InstallService start=disabled
+sc stop InstallService
